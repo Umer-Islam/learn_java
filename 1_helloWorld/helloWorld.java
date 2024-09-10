4fe22730bc35cedd6d888c6d196b34c468025818
umer islam <139012668+Umer-Islam@users.noreply.github.com>
2024-09-10 15:07:37 +0500

    upate

--- 1_helloWorld/helloWorld.java
+++ 1_helloWorld/helloWorld.java
@@ -1,6 +1,9 @@
+
 public class helloWorld {
     public static void main(String args[]) {
         System.out.println("hello world!");
-
+        System.out.println("whatever");
+        int a = 34;
+        System.out.println(b);
     }
 }
